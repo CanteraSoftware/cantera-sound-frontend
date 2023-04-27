0d9df6205c5dcbf2e7df8e7aba57b47fbcfc22e6
Axel Torrico <torricohernan2@gmail.com>
2023-04-27 16:21:36 -0300

    agregamos el componente Header y sus estilos

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "../styles/Header.css";
+
+//icons
+import { AiOutlineSearch } from "react-icons/ai";
+import { CgClose } from 'react-icons/cg'
+import logoApp from "../images/cat.png";
+
+export function Header() {
+  const [openSearch, setOpenSearch] = useState(false)
+
+  const handleClick = () => {
+    setOpenSearch(!openSearch)
+  }
+
+  return (
+    <div className="Header">
+      <div className="Header-title">
+        <div className="Header-content-title">
+          <img className="Header-logoApp" src={logoApp} alt="" />
+          <h1>
+            Cantera <span>Music</span>
+          </h1>
+        </div>
+        <div className={`Header-search ${openSearch && 'open'}`}>
+          <div className="Header-search-container">
+            <input className="Header-search-input" type="text" placeholder='Buscar' />
+            <div className="Header-search-icon-container">
+                <AiOutlineSearch className='Header-icons-search' />
+            </div>
+          </div>
+          <button
+            onClick={handleClick}
+            className={`bnt-closed ${openSearch && 'open'}`}
+            >
+              <CgClose className='Header-icons-search' />
+            </button>
+        </div>
+        <div onClick={handleClick}>
+          <AiOutlineSearch className="Header-icons-search" />
+        </div>
+      </div>
+    </div>
+  );
+}
