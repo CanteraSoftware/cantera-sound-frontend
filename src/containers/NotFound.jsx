3005710b850e1d11ab8a68db1371b0593dd96d0f
Axel Torrico <torricohernan2@gmail.com>
2023-04-27 16:04:50 -0300

    agregamos el componente NotFound y sus estilos (Fabian)

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/NotFound.css'
+
+
+import logoApp from '../images/cat.png';
+import imgError from '../images/pexels-lucas-pezeta-2930204.png'
+
+export function NotFound(){
+  return(
+    
+    <div className='NotFound-header'>
+      <div className="NotFound-header-title">
+        <div className="NotFound-header-content-title">
+          <img className="NotFound-header-logoApp" src={logoApp} alt="" />
+            <h1>Cantera <span>Music</span></h1>
+        </div>
+      </div>
+      <main>
+      <div className="NotFound-content-imgError">
+          <img className="NotFound-imgError" src={imgError} alt="gatoBurlon" />
+          <h1 className="NotFound-error-LineText">Oops!!<br/>Algo salió mal!!</h1>
+          <Link className="NotFound-error-volver" to="/">Volver</Link>
+      </div>
+      </main>
+    </div>
+  )
+}
