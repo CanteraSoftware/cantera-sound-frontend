263a8cf525a40fbe09dcb5625409392140d01ecd
Axel Torrico <torricohernan2@gmail.com>
2023-04-27 16:38:50 -0300

    agregamos el componente PlayerSlider y sus estilos (Facundo)

---
+++ src/components/Song.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import '../styles/Song.css'
+
+export function Song({title, artist, img}) {
+  return (
+    <div className='Song'>
+      <ul>
+        <li>
+          <Link className='Song-content' to='/player'>
+            <img src={img} alt="" />
+            <div className="Song-description">
+              <h4>{ title }</h4>
+              <p>{ artist }</p>
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
