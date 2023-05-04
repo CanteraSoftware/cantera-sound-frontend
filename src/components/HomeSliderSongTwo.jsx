09c11c8fe6bf07992b955b5375208ac4b73ee128
Axel Torrico <torricohernan2@gmail.com>
2023-05-04 15:32:35 -0300

    elimine la importacion de api.js que ya no existe

--- src/components/HomeSliderSongTwo.jsx
+++ src/components/HomeSliderSongTwo.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { api } from '../exports/api';
 import '../styles/HomeSliderSong.css'
 
 export function HomeSliderSongTwo({api}) {
