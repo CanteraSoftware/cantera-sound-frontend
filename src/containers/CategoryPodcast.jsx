e9ee78cf9d43f6edf40eb6a271e54c54c4ffd3bb
Axel Torrico <torricohernan2@gmail.com>
2023-05-08 12:22:19 -0300

    se cambio el orden de import

--- src/containers/CategoryPodcast.jsx
+++ src/containers/CategoryPodcast.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { DataFile } from '../components/DataFile';
 import { FooterMenu } from '../components/FooterMenu';
-import "../styles/Category.css";
 import { Loading } from '../components/Loading';
+import "../styles/Category.css";
 
 export function CategoryPodcast() {
   const url = 'http://18.117.98.49:5000/api/v1/categories/2'
