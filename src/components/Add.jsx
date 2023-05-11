0674690cb63220862355b31953cfab7dfce40f01
Axel Torrico <torricohernan2@gmail.com>
2023-05-11 10:56:59 -0300

    añadí la funcionalidad de agregar archivos a la api

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Add.css';
 
 export function Add({ see, notSee }) {
   const [nameFile, setNameFile] = useState('');
   const [nameAuthor, setNameAuthor] = useState('');
-  const [category, setCategory] = useState(null);
-  const [genres, setGenres] = useState(null);
-  /* const [file, setFile] = useState(null);
-  const [fileUrl, setFileUrl] = useState(''); */
+  const [category, setCategory] = useState('');
+  const [genres, setGenres] = useState('');
+  const [file, setFile] = useState(null);
+  const [fileUrl, setFileUrl] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [genders, setGenders] = useState([]);
+
+  useEffect(() => {
+    //obtengo las categorías
+    fetch('http://18.117.98.49:5000/api/v1/categories')
+      .then(response => response.json())
+      .then(data => setCategories(data))
+  }, []);
+
+  useEffect(() => {
+    //obtengo los géneros
+    fetch('http://18.117.98.49:5000/api/v1/genders')
+      .then(response => response.json())
+      .then(data => setGenders(data))
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Objeto que se enviará
+    //objeto que se enviará
     const formData = new FormData();
     formData.append('nameFile', nameFile);
     formData.append('nameAuthor', nameAuthor);
-    /* formData.append('file', file);
-    formData.append('fileUrl', fileUrl); */
+    formData.append('categoryId', category);
+    formData.append('genderId', genres);
+    formData.append('file', file);
+    formData.append('fileUrl', fileUrl);
 
     const url = 'http://18.117.98.49:5000/api/v1/files/upload';
 
@@ -25,16 +43,16 @@ export function Add({ see, notSee }) {
       method: 'POST',
       body: formData,
     })
-      .then((response) => response.text())
+      .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        /* setFileUrl(data.Location); */
+        setFileUrl(data.Location);
       });
   };
 
-  /* const handleFileInputChange = (e) => {
+  const handleFileInputChange = (e) => {
     setFile(e.target.files[0]);
-  }; */
+  };
 
   const handleClick = () => {
     notSee(false);
@@ -43,26 +61,15 @@ export function Add({ see, notSee }) {
   return (
     <>
       {see &&
-        <div className='Add'>
-          <div className="Add-container">
+        <div className='App'>
+          <div className="App-container">
             <h2>Agrega Tu Archivo</h2>
-            <form onSubmit={handleSubmit} className='Add-form'>
+            <form onSubmit={handleSubmit} className='App-content'>
 
-              <div className='Add-form-input'>
-                <label className='file'>Archivo</label>
-                <input
-                  className='Add-input'
-                  type="text"
-                  id='file'
-                  /* value={nameFile} */
-                  /* onChange={(e) => setNameFile(e.target.value)} */
-                />
-              </div>
-
-              <div className='Add-form-input'>
+              <div className='App-content-input'>
                 <label className='name'>Nombre</label>
                 <input
-                  className='Add-input'
+                  className='App-input'
                   type="text"
                   id='name'
                   value={nameFile}
@@ -70,10 +77,10 @@ export function Add({ see, notSee }) {
                 />
               </div>
 
-              <div className='Add-form-input'>
+              <div className='App-content-input'>
                 <label className='artist'>Artista / Autor</label>
                 <input
-                  className='Add-input'
+                  className='App-input'
                   type="text"
                   id='artist'
                   value={nameAuthor}
@@ -81,39 +88,61 @@ export function Add({ see, notSee }) {
                 />
               </div>
 
-              <div className='Add-form-input'>
+              <div className='App-content-input'>
                 <label className='category'>Categorías</label>
-                <select className='Add-input' >
+                <select
+                  className='App-input'
+                  onChange={(e) => setCategory(e.target.value)}
+                >
                   <option value="" disabled selected hidden></option>
-                  <option>Música</option>
-                  <option>Podcast</option>
-                  <option>Audio libro</option>
+                  {categories.map((category) => {
+                    return (
+                      <option
+                        key={category.id}
+                        value={category.id}
+                      >
+                        {category.nameCategory}
+                      </option>
+                    )
+                  })}
                 </select>
               </div>
 
-              <div className='Add-form-input'>
+              <div className='App-content-input'>
                 <label className='genres'>Géneros</label>
-                <select className='Add-input' >
+                <select
+                  className='App-input'
+                  onChange={(e) => setGenres(e.target.value)}
+                >
                   <option value="" disabled selected hidden></option>
-                  <option>Comedia</option>
-                  <option>Reggaeton</option>
-                  <option>Misterio</option>
+                  {genders.map((genres) => {
+                    return (
+                      <option
+                        key={genres.id}
+                        value={genres.id}
+                      >
+                        {genres.nameGender}
+                      </option>
+                    )
+                  })}
                 </select>
               </div>
 
-              <div className="Add-container-btn">
-                <button
-                  className='Add-btn-cancel'
-                  onClick={handleClick}
-                >
-                  Cancelar
-                </button>
-                <button
-                  type='submit'
-                  className='Add-btn-save'
-                >
-                  Guardar
-                </button>
+              <div className='App-content-input'>
+                <label className='file'>Archivo</label>
+                <input
+                  className='App-input-audio'
+                  type="file"
+                  name="uploads"
+                  accept=".mp3"
+                  multiple
+                  onChange={handleFileInputChange}
+                />
+              </div>
+
+              <div className="App-container-btn">
+                <button className='App-btn-cancel' onClick={handleClick}>Cancelar</button>
+                <button type='submit' className='App-btn-save'>Guardar</button>
               </div>
 
             </form>
