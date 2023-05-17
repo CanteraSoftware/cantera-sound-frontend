45af9980d24d95b3f3f28f0d319e5575ef7b473e
Axel Torrico <torricohernan2@gmail.com>
2023-05-17 12:03:08 -0300

    agregue un mensaje por si no se encuentra lo que busca y le di estilos al mensaje

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -71,19 +71,25 @@ export function Search() {
       </div>
       <div className="Search-container-suggesting">
         {search === '' ? <div></div> :
-          [<h2>Resultados de tu búsqueda:</h2>,
-          files.map(file => {
-            return (
-            <DataFile
-              key={file.id}
-              id={file.id}
-              img={file.imageUrl}
-              title={file.nameFile}
-              artist={file.nameAuthor}
-              catId={file.categoryId}
-            />
-          )
-        })]}
+          [
+            <h2>Resultados de tu búsqueda:</h2>,
+            files.length === 0 ? (
+              <h5>No tenemos el audio que buscas. <br />
+                Pero, <span>¡Puedes agregarlo!</span>
+              </h5>
+            ) : (
+              files.map((file) => (
+                <DataFile
+                  key={file.id}
+                  id={file.id}
+                  img={file.imageUrl}
+                  title={file.nameFile}
+                  artist={file.nameAuthor}
+                  catId={file.categoryId}
+                />
+              ))
+            )
+          ]}
         <h2>Sugerencias para ti</h2>
         {isloading ? <LoadingDataFile /> : suggesting.map(sugg => {
           return (
@@ -110,3 +116,63 @@ export function Search() {
     </div>
   );
 }
+
+
+
+
+/* import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { DataFile } from '../components/DataFile';
+import { LoadingDataFile } from '../components/LoadingDataFile';
+import { SearchPodcast } from '../components/SearchPodcast';
+import { SearchAudioBooks } from '../components/SearchAudioBooks';
+import { FooterMenu } from '../components/FooterMenu';
+import '../styles/Search.css'
+
+//icons
+import { AiOutlineSearch } from "react-icons/ai";
+import { CgClose } from 'react-icons/cg'
+
+export function Search() {
+  const [files, setFiles] = useState([]);
+  const [filesSearch, setFilesSearch] = useState([]);
+  const [suggesting, setSuggesting] = useState([]);
+  const [search, setSearch] = useState('');
+  const [isloading, setIsLoading] = useState(true);
+  const [noResults, setNoResults] = useState(false);
+
+  const url = "http://18.117.98.49:5000/api/v1/files";
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => {
+        data.sort(() => { return Math.random() - 0.5 })
+        setFiles(data)
+        setFilesSearch(data)
+        setSuggesting(data)
+        setIsLoading(false)
+      })
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+    filterSearch(e.target.value)
+  }
+
+  const filterSearch = (searchTerm) => {
+    let searchResults = filesSearch.filter((element) => {
+      if (
+        element.nameFile.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
+        element.nameAuthor.toString().toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        return element
+      }
+    })
+
+    setFiles(searchResults)
+    setNoResults(searchResults.length === 0);
+  }
+
+}
+ */
